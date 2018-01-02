c40a04623e690a5d654f3a915993d52bc3089bc7
Kaneki Kaku <maxgreg13625@users.noreply.github.com>
2018-01-02 09:52:18 +0800

    update image size if its width and height over 1500

--- imageWindow.py
+++ imageWindow.py
@@ -8,23 +8,24 @@ class ImageWindow(QtGui.QWidget):
 
 		self.setWindowTitle(title)
 		self.resize(300,200)
-
+		
 		#title is made by imageColumn_rowIndex
 		tempList=title.split('_')
 		rowIndex=tempList[len(tempList)-1]
-
+		
 		recordList=cf.readAvro(avroFileName)
 		self.imageLabel=QtGui.QLabel()
 		image=QtGui.QImage()
-		#assume all image are JPG type
 		image.loadFromData(QtCore.QByteArray.fromBase64(recordList[int(rowIndex)][str(tempList[0])]), imgType)
+		if image.width()>1500 or image.height()>1500:
+			image=image.scaled(image.width()/3, image.height()/3)
 		self.imageLabel.setPixmap(QtGui.QPixmap.fromImage(image))
 
 		#add scroll bar for image view
 		scroll=QtGui.QScrollArea()
 		scroll.setWidget(self.imageLabel)
 		scroll.setWidgeResizable=True
-
+	
 		verticalLayout=QtGui.QVBoxLayout()
 		verticalLayout.addWidget(scroll)
 		self.setLayout(verticalLayout)
